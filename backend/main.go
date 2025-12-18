aa3ac836404265a42eb5cd4b2d23ea2565d49277
Hamse Arwo <arwohamse@gmail.com>
2025-12-18 16:36:28 -0500

    Revert Previous Changes

--- backend/main.go
+++ backend/main.go
@@ -5,40 +5,38 @@ import (
 	// "runtime/trace"
 
 	db "investment_tracker/database"
-
+	"investment_tracker/initializer"
 	"investment_tracker/routes"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
+
 	db.InitDB()
-	// initializer.LoadEnvVariables()
+
+	initializer.LoadEnvVariables()
 
 }
 
 func main() {
 
 	router := gin.Default()
-	router.Use(CORSMiddleware())
+
+	router.Use(cors.New(cors.Config{
+
+		AllowOrigins: []string{"http://localhost:5173"},
+
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+
+		AllowCredentials: true,
+	}))
 
 	routes.SetupRoutes(router)
 
 	router.Run("localhost:8080")
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
